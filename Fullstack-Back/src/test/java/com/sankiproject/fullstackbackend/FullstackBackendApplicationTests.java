7f3bccb149761e4096d33e1113f090c9041d4349
sanki476 <131765957+sanki476@users.noreply.github.com>
2024-08-06 17:33:37 +0530

    Add files via upload

---
+++ Fullstack-Back/src/test/java/com/sankiproject/fullstackbackend/FullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sankiproject.fullstackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
